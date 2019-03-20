c5b9858bd7a79e32db0694bdb1dec6e24bb5747b
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2019-03-20 16:24:09 +0900

    #8.6 React Events and Typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,37 @@
 import React, { Component } from "react";
 import Number from "./Number";
+import { Form, Input } from "./Input";
 
 interface IState {
   counter: number;
+  name: string;
 }
 
 class App extends Component<{}, IState> {
   state = {
-    counter: 0
+    counter: 0,
+    name: ""
   };
   render() {
-    const { counter } = this.state;
+    const { counter, name } = this.state;
     return (
       <div>
+        <Form onFormSubmit={this.onFormSubmit}>
+          <Input value={name} onChange={this.onChange} />
+        </Form>
         <Number count={counter} /> <button onClick={this.add}>Add</button>
       </div>
     );
   }
+
+  onChange = (event: React.SyntheticEvent<HTMLInputElement>) => {
+    console.log(event.target);
+  };
+
+  onFormSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+  };
+
   add = () => {
     this.setState(prev => {
       return {
---
+++ src/Input.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+interface IInputProps {
+  value: string;
+  onChange: (event: React.SyntheticEvent<HTMLInputElement>) => void;
+}
+
+export const Input: React.FunctionComponent<IInputProps> = ({
+  value,
+  onChange
+}) => (
+  <input type="text" placeholder="Name" value={value} onChange={onChange} />
+);
+
+interface IFormProps {
+  onFormSubmit: (event: React.FormEvent) => void;
+}
+
+export const Form: React.FunctionComponent<IFormProps> = ({
+  children,
+  onFormSubmit
+}) => <form onSubmit={onFormSubmit}> {children}</form>;
